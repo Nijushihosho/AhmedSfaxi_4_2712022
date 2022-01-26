22fb70c2925dd38cd1449d43659735539c26b89f
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-01-26 14:40:41 +0100

    mentorat

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,18 +8,27 @@ function editNav() {
   }
 }
 
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".span.close");
 
-// launch modal event
+// launch modal event fonction fléchée
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//btnClose.addEventListener("click", closeModal);
+btnClose.addEventListener("click", (e) => {
+  console.log(e);
+} )
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal () {
+  modalbg.style.display = "none";
+}
 
