9dac151af746f094b51f8c198858ceca5f29b606
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-01-30 23:31:56 +0100

    formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,3 +32,12 @@ function closeModal () {
   modalbg.style.display = "none";
 }
 
+// Formulaire valide lable vers html for et 
+formData.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (value.startsWith('Hello ')) {
+      isValid = true;
+  } else {
+      isValid = false;
+  }
+});
