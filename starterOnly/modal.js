eb34ff0610547f85f30e1688ad8e61073e4460d1
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-02-08 19:26:13 +0100

    html  + const

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,13 +13,36 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnClose = document.querySelector("span.close");
+const firstName = document.getElementById("#first");
+const lastName = document.getElementById("last");
+const mail = document.getElementById("email");
+const birthdate = document.getElementById("birthDate");
+
+// Const de vérification
+/*const.inpats  = (value) => {
+  console.log(value);
+  if (value.length > 0 && (value.length < 2)
+  {
+    errorDisplay("textContent=(Ce champ doit au moins contenir 2 lettres");
+  });
+});
+
+
+
+const firstNameChecker = (value) => {
+  console.log(value);
+};
+const birthdayChecker = () => {};
+const emailChecker = () => {};
+const confirmChecker = () => {};
+*/
 
 // launch modal event fonction fléchée
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 btnClose.addEventListener("click", closeModal);
 btnClose.addEventListener("click", (e) => {
   console.log(e);
-} )
+});
 
 // launch modal form
 function launchModal() {
@@ -32,3 +55,12 @@ function closeModal () {
 }
 
 // lier les labels aux entrées dans le HTML validé par Submit
+
+firstName.forEach((text) => {
+  firstName.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "first":
+        firstNameChecker(e.target.value);
+        break;
+    };
+  });
