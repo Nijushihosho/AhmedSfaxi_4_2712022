1d0a1de2113034c97b5636703b326c98470bc4d0
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-02-11 20:53:23 +0100

    function validate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,54 +14,6 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnClose = document.querySelector("span.close");
 
-// Pour chaque champ, définition des contraintes
-
-function check(){
-  //pour chaque champ vérifie les contraintes
-const constraints = {
-  last : ['[a-z]{2,}$' , "Le nom doit comporter un minimum de 2 caractères"],
-  first :['[a-z]{2,}$' , "Le prénom doit comporter un minimum de 2 caractères"],
-}
-};
-
-// Lit l'id des champs
-
-const firstName = document.getElementById("first").value;
-console.log("j'affiche le ",firstName);
-//const lastName = document.getElementById("last").value;
-//const mail = document.getElementById("email").type;
-//const birthdate = document.getElementById("birthDate").type;
-
-
-
-// Build the constraint checker
-//const constraint = new RegExp(constraints[firstName][0]("");
-//console.log(constraint);
-
-
-// Check it!
-//if (constraint.test(firstName.value)) {
-  // The first the constraint, we use the ConstraintAPI to tell it
- // firstName.setCustomValidity("");
-//}
-//else {
-  // The firstname doesn't follow the constraint, we use the ConstraintAPI to
-  // give a message about the format required for this firstName
-//firstName.setCustomValidity(constraints[first][1]);
-//}
-//}
-
-//Then we link it to the oninput event for the <input>:
-
-window.onload = function () {
-document.getElementById("first").oninput = check;
-//document.getElementById("last").oninput = check;
- // document.getElementById("email").oninput = check;
-//document.getElementById("birthdate").oninput = check;
-}
-
-
-
 
 // launch modal event fonction fléchée
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -80,6 +32,58 @@ function closeModal () {
   modalbg.style.display = "none";
 }
 
+
+// Pour chaque champ du formulaire, définition des contraintes de API 
+
+function check(){
+  //pour chaque champ du formulaire, vérifie les contraintes
+let constraints = {
+last : ['/[a-z]{2,}$\.test(value);' , "Veuillez entrer 2 caractères ou plus pour le champ du nom"],
+first : ['/[a-z]{2,}$\.test(value);' , "Veuillez entrer 2 caractères ou plus pour le champ du prénom"],
+checkbox : ['', "Vous devez choisir une option"],
+checkbox1 : ['checked', "Vous devez vérifiez que vous acceptez les termes et conditions"],
+birthdate : ['', "Vous devez entrer votre date de naissance"],
+}
+};
+
+// Lit l'id des champs et y récupère la <value>
+
+const firstName = document.getElementById("first").value;
+console.log("j'affiche le ",firstName);
+const lastName = document.getElementById("last").value;
+const mail = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+
+
+// Build the constraint checker
+let constraint = new RegExp(constraints[firstName][0],"");
+console.log(constraint);
+
+
+// Check it!
+function validate(inputID) {
+  const input = document.getElementById(inputID);
+  const validityState = input.validity;
+if (constraints.test(firstName.value)) {
+  // The first the constraint, we use the Constraint checkfunction API to tell it
+firstName.setCustomValidity("Merci ! Votre réservation a été reçue.");
+}
+else {
+ //The firstname doesn't follow the constraint, we use the checkfunction API to
+  // give a message about the format required for this firstName
+firstName.setCustomValidity(constraints[firstName][1],"Veuillez entrer 2 caractères ou plus pour le champ du prénom");
+}
+
+input.reportValidity();
+}
+//Then we link it to the oninput event for the <input>:
+
+window.onload = function () {
+document.getElementById("first").oninput = check;
+document.getElementById("last").oninput = check;
+document.getElementById("email").oninput = check;
+document.getElementById("birthdate").oninput = check;
+}
 
 
 
