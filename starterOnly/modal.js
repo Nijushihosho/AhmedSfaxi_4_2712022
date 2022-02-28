f94e58e369bff122b19b329fce89b1d65efac3c1
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-02-28 23:59:22 +0100

    check

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+const { validate } = require("json-schema");
 const { resourceUsage } = require("process");
 
 function editNav() {
@@ -38,75 +39,59 @@ function closeModal () {
 
 const firstName = document.getElementById("first").value;
 const lastName = document.getElementById("last").value;
-const mail = document.getElementById("email");
-const bDate = document.getElementById("birthdate");
-const checkBox1 = document.getElementsByTagName(location).value;
+const mail = document.getElementById("email").value;
+const bDate = document.getElementById("birthdate").value;
+const radio = document.createElement("INPUT");
+radio.setAttribute("name", "type");
 const checkBox = document.getElementById("checkbox");
 
 // Pour chaque champ du formulaire, définition des contraintes de API 
 
-function check(){
+function check() {
   //pour chaque champ du formulaire, vérifie les contraintes
-let validateAll = true, {
-  if (document.getElementById("first").value == "") {
-    firstName.style.borderColor = "#FF0000";
+let validateAll = true; 
+   if (firstName == "") {
     alert ("Veuillez entrer 2 caractères ou plus pour le champ du nom");
-    validateAll = false;
+    firstName.style.borderColor = "#FF0000";
+    return false;
   } else {
-    document.getElementById("first").value="Reset"; 
+    (document.getElementById(firstName).style.borderColor=#FFF); 
   }
-  if (document.getElementById("last").value == "") {
+  if (lastName == "") {
     lastName.style.borderColor = "#FF0000";
     alert ("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
+    return false;
+  } else {
+    document.getElementById(lastName)="Reset"); 
+  }
+  if ("mail".value == "") {
+    mail.style.borderColor = "#FF0000";
+    alert ("Veuillez entrer une adresse mail conforme");
     validateAll = false;
   } else {
-    document.getElementById("last").value="Reset"; 
+    ("last".value="Reset"); 
   }
-  if (document.getElementById("bDate").value == "") {
+  if ("bDate".value == "") {
     bDate.style.borderColor = "#FF0000";
     alert ("Veuillez entrer une date au format date de naissance");
     validateAll = false;
   } else {
-    document.getElementById("bDate").value="Reset"; 
+    ("bDate".value="Reset"); 
   }
-  if(!checkBox) {
-document.getElementsByTagName("location").selectedIndex =0;
+  if(!radio) {
+radio.selectedIndex = "0";
     alert("Vous devez vérifiez que vous acceptez les termes et conditions");
     validateAll = false;
   }
   else {
-    document.getElementById("checkBox").value="Reset";
+    ("radio".type="Reset");
+}
 }
 
-}
 
 
 /*firstName : ["/^[a-z]{2,}$/" , "Veuillez entrer 2 caractères ou plus pour le champ du prénom"],
 lastName : ["/^[a-z]{2,}$/" , "Veuillez entrer 2 caractères ou plus pour le champ du nom"],
 mail : ["/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"],
 bDate : ["/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/", "Vous devez entrer votre date de naissance"],
-};*/
-
-
-  
-//Then we link it to the oninput event for the <input>:
-
-validateAll = function (check) {
-document.getElementById("first").onchange = check;
-document.getElementById("last").oninput = check;
-document.getElementById("mail").oninput = check;
-document.getElementById("bDate").oninput = check;
-}
-
-/*validation si remplit contraintes de check sinon désactive le submit 
-function validate()
-{
-  const form = target;
-
-  if (!form.check()) {
-
-    // form is invalid - cancel submit
-    preventDefault();
-    stopImmediatePropagation();
-  }
-};*/
+;*/
