a142366f7c2a7572a0a339c3c80f564bc251f3e1
Ahmed Sfaxi <nijushihosho@gmail.com>
2022-03-08 15:35:36 +0100

    FormData Begins

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -40,8 +40,7 @@ console.log(firstName);
 const lastName = document.getElementById("last").value;
 const mail = document.getElementById("email").value;
 const bDate = document.getElementById("birthdate").value;
-const radio = document.createElement("INPUT");
-radio.setAttribute("name", "type");
+const radio = document.getElementById("location1").value;
 const checkBox = document.getElementById("checkbox");
 
 // Pour chaque champ du formulaire, définition des contraintes de API 
@@ -50,54 +49,50 @@ function check() {
   //pour chaque champ du formulaire, vérifie les contraintes
 let validateAll = true; 
    if (firstName.match(/^[a-z]{2,}$/))  {
-   console.log("match!");
-    document.getElementById("first")=("Veuillez entrer 2 caractères ou plus pour le champ du nom");
+    console.log("match");
+    FormData("Veuillez entrer 2 caractères ou plus pour le champ du nom");
     firstName.style.borderColor = "#FF0000";
-    validateAll = false;
+    return false;
   } else {
     firstName.style.borderColor = "#FF0000".reset();
-    document.getElementById("first").innerHTML=("Veuillez entrer 2 caractères ou plus").reset();
+    data-error.("Veuillez entrer 2 caractères ou plus").reset();
   }
-  if (lastName.match(/^[a-z]{2,}$/))  {
-    console.log("match!");
-    document.getElementById("last").innerHTML=("Veuillez entrer 2 caractères ou plus pour le champ du prénom")
+  if (lastName.match(/^[a-z]{2,}$/))  { 
+    console.log("match");
+    alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom")
     lastName.style.borderColor = "#FF0000";
-  validateAll = false;
+  return false;
   } else {
     lastName.style.borderColor = "#FF0000".reset();
-    document.getElementById("last").innerHTML=("Veuillez entrer 2 caractères ou plus").reset();
+    alert("Veuillez entrer 2 caractères ou plus").reset();
   }
-  if (mail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
-    mail.style.borderColor = "#FF0000";
-    document.getElementById("email").innerHTML=("Veuillez entrer une adresse mail conforme");
-    validateAll = false;
+  if (mail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+    alert("Veuillez entrer une adresse mail conforme");
+    return false;
   } else {
     lastName.style.borderColor = "#FF0000".reset();
-    document.getElementById("email").innerHTML=("Veuillez entrer une adresse mail conforme").reset();
+    alert("Veuillez entrer une adresse mail conforme").reset();
   }
-    if (bDate.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/) {console.log("match");
-    bDate.style.borderColor = "#FF0000";
-    document.getElementById("birthdate").innerHTML=("Veuillez entrer votre date de naissance");
-    validateAll = false;
+    if (bDate.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/)) {
+    alert("Veuillez entrer votre date de naissance");
+    return false;
   } else {
     lastName.style.borderColor = "#FF0000".reset();
-    document.getElementById("birthdate").innerHTML=("Veuillez entrer votre date de naissance").reset();
+   alert("Veuillez entrer votre date de naissance").reset();
   }
   /*for(i=0 < i=)*/
-  if(radio) {
-    document.getElementById("INPUT").checked=false;
-    document.getElementById("radio").innerHTML=("Vous devez vérifier que vous acceptez les termes et conditions");
-    validateAll = false;
-  }
-  else {
+  if (radio.checked==false) {
+    alert("Vous devez vérifier que vous acceptez les termes et conditions");
+    return false;
+  } else {
     lastName.style.borderColor = "#FF0000".reset();
-    document.getElementById("radio").innerHTML=("Vous devez vérifier que vous acceptez les termes et conditions").reset();
+    alert("Vous devez vérifier que vous acceptez les termes et conditions").reset();
   }
-}
+
 
 check();
 
-
+}
 
 
 /*firstName : ["/^[a-z]{2,}$/" , "Veuillez entrer 2 caractères ou plus pour le champ du prénom"],
